d23bda6f092d35deea272c7c989ba8442766c8d9
escalepion <escalepion@gmail.com>
2017-10-14 18:20:24 +0300

    add testing insert-remove

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -6,6 +6,17 @@ import { Notes } from './notes';
 if (Meteor.isServer) {
     describe('notes', function () {
 
+    beforeEach(function () {
+        Notes.remove({});
+        Notes.insert({
+            _id: 'testNoteId1',
+            title: 'My title',
+            body: 'My body for note',
+            updatedAt: 0,
+            userId: 'testUserId'
+        });
+    });
+
     it('should insert new note', function () {
         const userId = 'testid';
         const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
@@ -17,6 +28,24 @@ if (Meteor.isServer) {
     it('should not insert note if not uthenticated', function() {
         expect(() => {
             Meteor.server.method_handlers['notes.insert']();
+        }).toThrow();
+    });
+
+    it('should remove not', function () {
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId' }, ['testNoteId1']);
+
+        expect(Notes.findOne({ _id: 'testNoteId1' })).toNotExist();
+    });
+
+    it('should not remove note if not outhenticated', function () {
+        expect(() => {
+            Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1']);
+        }).toThrow();
+    });
+
+    it('should not remove note if invalid id', function () {
+        expect(() => {
+            Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId' });
         }).toThrow();
     });
 
