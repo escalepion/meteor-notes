655d2bf4739a4c432202de3871b01781c7b2ae7b
escalepion <escalepion@gmail.com>
2017-10-15 16:46:43 +0300

    testing publications

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -12,10 +12,18 @@ if (Meteor.isServer) {
         updatedAt: 0,
         userId: 'testUserId'
     };
+    const noteTwo = {
+        _id: 'testNoteId2',
+        title: 'Things to buy',
+        body: 'Note 2 body',
+        updatedAt: 0,
+        userId: 'testUserId2'
+    };
 
     beforeEach(function () {
         Notes.remove({});
         Notes.insert(noteOne);
+        Notes.insert(noteTwo);
     });
 
     it('should insert new note', function () {
@@ -102,6 +110,21 @@ if (Meteor.isServer) {
             Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId });
         }).toThrow();
     });
+
+    it('should return a users notes', function () {
+       const res = Meteor.server.publish_handlers.notes.apply({ userId: noteOne.userId });
+       const notes = res.fetch();
+
+       expect(notes.length).toBe(1);
+       expect(notes[0]).toEqual(noteOne);
+    });
+
+    it('should return zero notes for user that has none', function () {
+        const res = Meteor.server.publish_handlers.notes.apply({ userId: 'someid' });
+        const notes = res.fetch();
+ 
+        expect(notes.length).toBe(0);
+     });
 
     });
 }
