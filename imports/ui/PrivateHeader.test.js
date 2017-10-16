66a72afbc363d63729dda56e2da5fa673feca25a
escalepion <escalepion@gmail.com>
2017-10-16 14:06:30 +0300

    test functions with spy and simulate them

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -8,7 +8,7 @@ import PrivateHeader from './PrivateHeader';
 if(Meteor.isClient) {
     describe('PrivateHeadaer', function () {
         it('should set button text to logout', function () {
-            const wrapper = mount(<PrivateHeader title='text title' />);
+            const wrapper = mount(<PrivateHeader title='text title' handleLogout={() => {}} />);
             const buttonText = wrapper.find('button').text();
 
             expect(buttonText).toBe('Logout');
@@ -16,10 +16,26 @@ if(Meteor.isClient) {
 
         it('should use title prop as h1 text', function () {
             const title = 'This is sparta';
-            const wrapper = mount(<PrivateHeader title={title} />);
+            const wrapper = mount(<PrivateHeader title={title} handleLogout={() => {}} />);
             const titleText = wrapper.find('h1').text();
-
+            
             expect(titleText).toBe(title);
+        });
+
+        it('should trigger spy function', function() {
+            const spy = expect.createSpy();
+            spy(3,4);
+            spy('akif');
+
+            expect(spy).toHaveBeenCalledWith(3,4);
+        });
+
+        it('should call handleLogout on click', function () {
+            const spy = expect.createSpy();
+            const wrapper = mount(<PrivateHeader title='text title' handleLogout={spy} />);
+
+            wrapper.find('button').simulate('click');
+            expect(spy).toHaveBeenCalled();
         });
 
     });
