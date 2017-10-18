a6cefcb1a8682c75b9cf03500d04a6ee3b25da68
escalepion <escalepion@gmail.com>
2017-10-18 21:23:54 +0300

    render empty list item if no notes

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -5,13 +5,16 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Notes } from '../api/notes';
 import NoteListHEader from './NoteListHeader';
 import NoteListItem from './NoteListItem';
+import NoteListEmptyItem from './NoteListEmptyItem';
 
 export const NoteList = (props) => {
     return (
         <div>
             <NoteListHEader />
-            NoteList component { props.notes.length }
-            {
+          {console.log(props.notes)}
+            {   
+                props.notes.length < 1 ?
+                <NoteListEmptyItem /> :
                 props.notes.map((note) => {
                     return <NoteListItem key={note._id} note={note} />;
                 })
