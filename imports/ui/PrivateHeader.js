c8e74c1c31ffd13a5463441ed137cbc90c72edd5
escalepion <escalepion@gmail.com>
2017-10-23 18:45:52 +0300

    setup mobile navigation

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const PrivateHeader = (props) => {
+    const navImageSource = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
     // bu şekilde de olur 
     // const onLogout = () => {
     //     Accounts.logout();
@@ -10,6 +12,7 @@ export const PrivateHeader = (props) => {
     return (
         <div className="header">
             <div className="header__content">
+            <img className="header__nav-toggle" onClick={props.handleNavToggle} src={navImageSource} alt="bars"/>
                 <h1 className="header__title">{props.title}</h1>
                 <button className="button button--link--text" onClick={() => props.handleLogout()}>Logout</button>
             </div>
@@ -19,7 +22,9 @@ export const PrivateHeader = (props) => {
 
 export default createContainer(() => {
     return {
-        handleLogout: () => Accounts.logout()
+        handleLogout: () => Accounts.logout(),
+        handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+        isNavOpen: Session.get('isNavOpen')
     };
 }, PrivateHeader);
 
